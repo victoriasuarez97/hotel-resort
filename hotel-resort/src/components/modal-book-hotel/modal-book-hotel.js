bcb2a560971ca157dfd0fd17f60e5cb04bf82b92
victoriasuarez97 <victoriasuarez97@gmail.com>
2021-03-29 11:19:35 -0300

    Added: Book hotel success modal

--- hotel-resort/src/components/modal-book-hotel/modal-book-hotel.js
+++ hotel-resort/src/components/modal-book-hotel/modal-book-hotel.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import CoolFace from "../../assets/icons/cool.svg";
 
 import "./modal-book-hotel.scss";
 
@@ -17,6 +19,7 @@ export const ModalBookHotel = ({
   dateFromChosen,
   dateToChosen,
 }) => {
+  const [book, setBook] = useState(false);
   const closeModal = () => {
     setShowModal(false);
   };
@@ -35,18 +38,31 @@ export const ModalBookHotel = ({
   return (
     <div className={`modal-book-hotel-container ${showModal ? "open" : ""}`}>
       <div className="modal-book-hotel-wrapper">
-        <h3>¿Estás seguro de realizar esta reserva?</h3>
-        <p className="hotel-chosen">El hotel que elegiste es:</p>
-        <div className="hotel-chosen-data">
-          <p className="hotel-name">{hotel.name}</p>
-          <p>{`Fecha de Ingreso: ${formatUnix(dateFromChosen)}`}</p>
-          <p>{`Fecha de Ingreso: ${formatUnix(dateToChosen)}`}</p>
-          <p>{`En: ${hotel.city}, ${hotel.country}`}</p>
-        </div>
-        <div className="buttons-wrapper">
-          <button>Sí, quiero reservar</button>
-          <button onClick={closeModal}>No, me arrepentí</button>
-        </div>
+        {book ? (
+          <div className="booked-succesfully">
+            <img src={CoolFace} alt="cool emoji" />
+            <h3>¡La reserva se ha realizado con éxito!</h3>
+            <p>
+              Pronto te enviaremos un mail con toda la información de tu reserva
+            </p>
+            <button onClick={() => setShowModal(false)}>Aceptar</button>
+          </div>
+        ) : (
+          <>
+            <h3>¿Estás seguro de realizar esta reserva?</h3>
+            <p className="hotel-chosen">El hotel que elegiste es:</p>
+            <div className="hotel-chosen-data">
+              <p className="hotel-name">{hotel.name}</p>
+              <p>{`Fecha de Ingreso: ${formatUnix(dateFromChosen)}`}</p>
+              <p>{`Fecha de Ingreso: ${formatUnix(dateToChosen)}`}</p>
+              <p>{`En: ${hotel.city}, ${hotel.country}`}</p>
+            </div>
+            <div className="buttons-wrapper">
+              <button onClick={() => setBook(true)}>Sí, quiero reservar</button>
+              <button onClick={closeModal}>No, me arrepentí</button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
